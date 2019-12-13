1eb2d6641561d1bba34f4fb52567a9872e75fff9
Charles Kennedy <charlesscottkennedy@googlemail.com>
2019-12-13 11:26:40 +0000

    created Paddock class

--- src/main/java/com/codeclan/example/JurassicJapes/models/Paddock.java
+++ src/main/java/com/codeclan/example/JurassicJapes/models/Paddock.java
@@ -1,9 +1,55 @@
 package com.codeclan.example.JurassicJapes.models;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name="paddocks")
 public class Paddock {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="name")
+    private String name;
+
+    @JsonIgnoreProperties("paddocks")
+    @OneToMany(mappedBy = "paddock", fetch=FetchType.LAZY)
+    private List<Dinosaur> dinosaurs;
+
+    public Paddock(String name) {
+        this.name = name;
+        this.dinosaurs = new ArrayList<Dinosaur>();
+    }
+
+    public Paddock(){
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Dinosaur> getDinosaurs() {
+        return dinosaurs;
+    }
+
+    public void setDinosaurs(List<Dinosaur> dinosaurs) {
+        this.dinosaurs = dinosaurs;
+    }
 }
