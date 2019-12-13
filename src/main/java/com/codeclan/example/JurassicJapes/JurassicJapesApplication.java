d2882bf802bc9f7b93365b10384c12e25776ad3d
Charles Kennedy <charlesscottkennedy@googlemail.com>
2019-12-13 10:59:19 +0000

    initial commit

---
+++ src/main/java/com/codeclan/example/JurassicJapes/JurassicJapesApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.JurassicJapes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JurassicJapesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JurassicJapesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeclan/example/JurassicJapes/JurassicJapesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.JurassicJapes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JurassicJapesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
