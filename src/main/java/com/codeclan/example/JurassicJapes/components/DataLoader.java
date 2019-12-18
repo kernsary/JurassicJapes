90c8aba2df973ae075fa4a4f4f302f193e153407
Charles Kennedy <charlesscottkennedy@googlemail.com>
2019-12-18 16:39:13 +0000

    added more data

--- src/main/java/com/codeclan/example/JurassicJapes/components/DataLoader.java
+++ src/main/java/com/codeclan/example/JurassicJapes/components/DataLoader.java
@@ -26,11 +26,38 @@ public class DataLoader implements ApplicationRunner {
         Paddock swamp = new Paddock("Swamp");
         paddockRepository.save(swamp);
 
+        Paddock desert = new Paddock("Desert");
+        paddockRepository.save(desert);
+
+        Paddock jungle = new Paddock("Jungle");
+        paddockRepository.save(jungle);
+
         Dinosaur roary = new Dinosaur("T Rex", "meat", swamp);
         dinosaurRepository.save(roary);
 
-        Dinosaur garry = new Dinosaur("T Rex", "meat", swamp);
+        Dinosaur garry = new Dinosaur("Bronto", "plants", swamp);
         dinosaurRepository.save(garry);
+
+        Dinosaur karen = new Dinosaur("Bronto", "plants", swamp);
+        dinosaurRepository.save(karen);
+
+        Dinosaur mike = new Dinosaur("Bronto", "plants", swamp);
+        dinosaurRepository.save(mike);
+
+        Dinosaur dot = new Dinosaur("Bronto", "plants", jungle);
+        dinosaurRepository.save(dot);
+
+        Dinosaur dave = new Dinosaur("Bronto", "plants", jungle);
+        dinosaurRepository.save(dave);
+
+        Dinosaur jenny = new Dinosaur("Bronto", "plants", jungle);
+        dinosaurRepository.save(jenny);
+
+        Dinosaur jake = new Dinosaur("Raptor", "meat", desert);
+        dinosaurRepository.save(jake);
+
+        Dinosaur sue = new Dinosaur("Raptor", "meat", desert);
+        dinosaurRepository.save(sue);
 
     }
 }
